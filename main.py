8f1e2be02d9a7cc3c326d866fec9e0455adba4de
benglish00 <benglish00@users.noreply.github.com>
2017-11-07 18:25:14 -0700

    working CSV import

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import import_data
-import csv
 import numpy
 
 # Get Calibration matrix
-CalFile = import_data.pathname("Cal1430.csv")
+CalFile = import_data.pathname("RawCal1430.csv")
 CalMatrix = import_data.csv_to_array(CalFile)
 
 # Get Raw Data Matrix
 RawFile = import_data.pathname("Data1430.csv")
 RawMatrix = import_data.csv_to_array(RawFile)
-print(RawMatrix)
+print(CalMatrix)
+
+CalMatrix = CalMatrix[numpy.lexsort(numpy.fliplr(CalMatrix).T)]
+
+print(CalMatrix)

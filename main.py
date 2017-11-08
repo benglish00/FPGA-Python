e9831ded312e772fe2d435650bb2fae5e00dcd1c
benglish00 <benglish00@users.noreply.github.com>
2017-11-08 15:49:53 -0700

    Added function to calculate a polynomial fit at the amplitude peak.  Plotted amplitude and fit data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import import_data
 import calibrate
 import process
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Get Calibration matrix
 CalFile = import_data.pathname("RawCal1430.csv")
@@ -15,3 +17,11 @@ RawMatrix = import_data.csv_to_array(RawFile)
 RecMatrix = calibrate.rectify(CalAvg,RawMatrix)
 Peaks = process.poly_max(RecMatrix)
 print(Peaks)
+
+
+#Plotting
+polyfit = process.poly_data(RecMatrix)
+plt.plot(RecMatrix[:,0],RecMatrix[:,2],'ro', polyfit[:,0], polyfit[:,1],'b', linewidth=8)
+plt.ylabel('Envelope []')
+plt.xlabel('Frequency [MHz]')
+plt.show()
--- process/__init__.py
+++ process/__init__.py
@@ -12,7 +12,7 @@ def set_window(center, length):
     :param center: int
     :return: window (int1, int2)
     """
-    winsize = length//10
+    winsize = length//8
     halfwin = winsize//2
     if center < halfwin:
         window = [0, winsize - 1]
@@ -66,3 +66,37 @@ def poly_max(Matrix, column=2):
                + amp_polyfit[1]*freq_peak \
                + amp_polyfit[2]
     return [freq_peak, fft_peak, amp_peak]
+
+def poly_coeff(Matrix, column=2):
+    """
+    windowed 2nd order fit
+    :param Matrix: (m x n) float
+    :param column: int
+    :return: window indices and polyfit coefficients
+    """
+    index = np.argmax(Matrix[:, column])
+    window = set_window(index, len(Matrix))
+    freq_slice = Matrix[window[0]:window[1],
+                 0]
+    amp_slice = Matrix[window[0]:window[1],
+                2]
+    amp_polyfit = np.polyfit(freq_slice,
+                             amp_slice,
+                             2)
+    return [window, amp_polyfit]
+
+def poly_data(Matrix, column=2):
+    index = np.argmax(Matrix[:, column])
+    window = set_window(index, len(Matrix))
+    coeff = poly_coeff(Matrix)[1]
+    polyfit = np.zeros((window[1]-window[0],2))
+    m=0
+    for i in range (window[0],window[1]):
+        x = Matrix[i,0]
+        y = coeff[0]*x**2+coeff[1]*x+coeff[2]
+        polyfit[m,0]=x
+        polyfit[m,1]=y
+        m +=1
+    return polyfit
+
+

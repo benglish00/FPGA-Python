913628a7cbef0383341d6fbd4967066601600d0d
benglish00 <benglish00@users.noreply.github.com>
2017-11-08 10:54:34 -0700

    Rectify Data

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ CalFile = import_data.pathname("RawCal1430.csv")
 CalMatrix = import_data.csv_to_array(CalFile)
 CalAvg = calibrate.average(CalMatrix)
 
-
 # Get Raw Data Matrix
 RawFile = import_data.pathname("Data1430.csv")
 RawMatrix = import_data.csv_to_array(RawFile)
 
-# print((CalMatrix[0:10,0:3]))
